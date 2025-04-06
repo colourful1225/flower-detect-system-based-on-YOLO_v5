64e9d9195a51794dc8bba55b4abf0676ddbf0b5e
colourful1225 <chencankai1225@gmail.com>
2025-04-06 19:58:37 +0800

    the first time

---
+++ datasets/Flower/label_name.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+Chinese_name = {'15': "15", 'Allium': "大葱花", 'Borage': "琉璃苣", 'Burdock': "牛蒡", 'Calendula': "金盏花",
+                'Chicory': "菊苣", 'Chive blossoms': "韭菜花", 'Coltsfoot': "款冬", 'Common mallow': "锦葵",
+                'Common yarrow': "洋蓍草", 'Coneflower': "金光菊", 'Cow parsley': "欧芹", 'Dandelion': "蒲公英",
+                'Gardenia': "栀子花"}
+Label_list = ["大葱花", "琉璃苣", "牛蒡", "金盏花",
+              "菊苣", "韭菜花", "款冬", "锦葵",
+              "洋蓍草", "金光菊", "欧芹", "蒲公英",
+              "栀子花"]
